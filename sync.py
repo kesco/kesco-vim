807789846fc97ca1cd3f1d678373bc2ca0056f23
Kesco Lin <kesco915@gmail.com>
2015-01-29 14:42:59 +0800

    2015-01-29 14:42:59.800282

--- sync.py
+++ sync.py
@@ -2,8 +2,20 @@
 # -*- coding: utf-8 -*-
 
 import os
+from datetime import datetime
+import shutil
+from os.path import expanduser
 
 from git import Repo
 
 repo = Repo(os.getcwd())        # 生成Git Repo对象
-assert not repo.bare
+
+vimrc_path = expanduser('~/.vimrc')
+shutil.copyfile(vimrc_path, u'vimrc')
+
+index = repo.index
+index.add([u'vimrc', u'sync.py'])
+index.commit(str(datetime.now()))
+
+origin = repo.remotes.origin    # 获取远程Origin分支对象
+origin.push()                   # Push commit
